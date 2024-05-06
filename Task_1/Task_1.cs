21f523e74cbb0a0a344f49cda01e2aeb77d48d85
Ilia <isinetov@mail.ru>
2024-05-06 15:42:43 +0400

    Задание 1. Создайте свой тип исключения. Сделайте массив из пяти различных видов исключений, включая собственный тип исключения. Реализуйте конструкцию TryCatchFinally, в которой будет итерация на каждый тип исключения (блок finally по желанию). В блоке catch выведите в консольном сообщении текст исключения.

---
+++ Task_1/Task_1.cs
@@ -0,0 +1,27 @@
+﻿class UserInputException : Exception 
+{
+    
+}
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        Exception[] exceptions = {new FormatException(), new OverflowException(), new UserInputException(), new KeyNotFoundException(), new NotImplementedException()};
+        for (int i = 0; i < exceptions.Length;)
+        {
+            try
+            {
+                throw exceptions[i];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                i++;
+            }
+        }
+    }
+}
